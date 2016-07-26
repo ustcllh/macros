fc09f1738b2ce9341c78abfef75217beaf41ed88
Jin Huang <jhuang@bnl.gov>
2016-07-26 11:14:44 -0400

    use MAPS + TPC

--- macros/g4simulations/G4Setup_sPHENIX.C
+++ macros/g4simulations/G4Setup_sPHENIX.C
@@ -20,11 +20,11 @@ void G4Init(bool do_svtx = true,
     }  
   if (do_svtx)
     {
-      gROOT->LoadMacro("G4_Svtx.C");                 // default MIE projections
+//      gROOT->LoadMacro("G4_Svtx.C");                 // default MIE projections
       //gROOT->LoadMacro("G4_Svtx_pixels+strips.C"); // testing
       //gROOT->LoadMacro("G4_Svtx_pixels+tpc.C");    // testing
       //gROOT->LoadMacro("G4_Svtx_maps+strips.C");   // testing
-      //gROOT->LoadMacro("G4_Svtx_maps+tpc.C");      // testing
+      gROOT->LoadMacro("G4_Svtx_maps+tpc.C");      // testing
       //gROOT->LoadMacro("G4_Svtx_maps_7layers.C");  // testing
       //gROOT->LoadMacro("G4_Svtx_maps_5layers.C");  // testing
       //gROOT->LoadMacro("G4_Svtx_ladders.C");       // testing (new geometries)
